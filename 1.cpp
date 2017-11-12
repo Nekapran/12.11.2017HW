5438a467ae3799e9928ed10c29973ff23e68643a
Nekapran <32580080+Nekapran@users.noreply.github.com>
2017-11-12 23:56:35 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+# 12.11.2017HW
+#include <iostream>
+
+using namespace std;
+int main()
+{
+ int i, a;
+  cout << "Input massive length: ";
+  cin >> i;
+  int mas[i];
+  for (int a = 0; a < i; a++)
+    {
+      cout << "[" << a + 1 << "]" << ": ";
+      cin >> mas[a];
+    }
+  cout << "\nYour massive: ";
+  for (int a = 0; a < i; ++a)
+    {
+      cout << mas[a] << " ";
+    }
+  cout << endl;
+  return 0;
+}
